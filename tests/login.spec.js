206cd5411c31067211be23ceddec68af6751e421
Simon <sstur@me.com>
2023-05-25 11:40:55 -0700

    Setup for task 3

---
+++ tests/login.spec.js
@@ -0,0 +1,6 @@
+test('should login', async ({ page }) => {
+  await page.goto('https://insta.livepreview.org/login');
+
+  // Task 3
+  // Write some locator and assertions to confirm that we are on the login page
+});
