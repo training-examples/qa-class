89b81f20265034cf0746b0407d44121b4362f9a7
Simon <sstur@me.com>
2023-05-25 13:33:40 -0700

    Test Mailchimp Login

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,19 +1,21 @@
 import { test, expect } from '@playwright/test';
 
 test('should login', async ({ page }) => {
-  await page.goto('https://mnog2f-5173.csb.app/login');
+  await page.goto('https://login.mailchimp.com/');
 
-  const inputUsername = page.getByLabel('Username');
+  const inputUsername = page.getByLabel('Username or Email');
   await expect(inputUsername).toBeVisible();
-  await inputUsername.fill('test');
+  await inputUsername.fill(process.env.MAILCHIMP_USERNAME);
 
   const inputPassword = page.getByLabel('Password');
   await expect(inputPassword).toBeVisible();
-  await inputPassword.fill('123');
+  await inputPassword.fill(process.env.MAILCHIMP_PASSWORD);
 
-  const button = page.getByRole('button');
-  await expect(button).toHaveText('Login');
+  const button = page.getByRole('button', { name: 'Log in' });
+  await expect(button).toContainText('Log in');
   await button.click();
 
-  await expect(page).toHaveURL('https://mnog2f-5173.csb.app/');
+  await expect(page).toHaveURL(
+    'https://us21.admin.mailchimp.com/login/verify/',
+  );
 });
