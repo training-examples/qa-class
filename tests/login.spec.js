5a267c306d4ead2fbf64aae96d942a384c436ace
Simon <sstur@me.com>
2023-05-25 12:14:07 -0700

    Task 3 solution

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,6 +1,20 @@
+import { test, expect } from '@playwright/test';
+
 test('should login', async ({ page }) => {
   await page.goto('https://insta.livepreview.org/login');
 
-  // Task 3
-  // Write some locator and assertions to confirm that we are on the login page
+  const inputUsername = page.getByLabel('Username');
+  await expect(inputUsername).toBeVisible();
+
+  const inputPassword = page.getByLabel('Password');
+  await expect(inputPassword).toBeVisible();
+
+  const button = page.getByRole('button');
+  await expect(button).toHaveText('Login');
+
+  const signupLink = page.getByRole('link');
+  await expect(signupLink).toHaveText('Sign up');
+
+  await signupLink.click();
+  await expect(page).toHaveURL(/.*signup/);
 });
