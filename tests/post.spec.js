99c4eddd55037b6da7b2f99116d7a67f139b1ebc
Simon <sstur@me.com>
2023-05-26 11:34:36 -0700

    Setup for task 8

---
+++ tests/post.spec.js
@@ -0,0 +1,10 @@
+// Task 8
+// Write a test to create a new post.
+// Steps:
+// - Login (we have a helper for this)
+// - Find the header button and click it
+// - This will navigate to the "/posts/new" page
+// - Find the <input> element with id file-picker
+// - Use the action setInputFiles() to select an image (you will need to find your own image and put it in the project directory)
+// - Fill out the comment field
+// - Submit the form
