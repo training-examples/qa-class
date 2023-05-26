970b0de93ba2217c217764e656ca2bb539ef2664
Simon <sstur@me.com>
2023-05-26 06:28:59 -0700

    Setup for task 5

---
+++ tests/home.spec.js
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('should render home feed', async ({ page }) => {
+  // Task 5
+  // First you'll need to login before you can test the home feed.
+  // You can copy/paste the login logic from the login.spec.js
+  // Then make some assertions about the home feed, without interacting with it.
+});
