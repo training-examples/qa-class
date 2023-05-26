c89b3a7ca7c79c595992616421cf2fecb9b0169e
Simon <sstur@me.com>
2023-05-26 11:21:44 -0700

    Also test button click

--- tests/comment.spec.js
+++ tests/comment.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { loginFlow } from './helpers/login.js';
 
-test('add comment', async ({ page }) => {
+test('add comment (enter press)', async ({ page }) => {
   await loginFlow(page);
 
   const firstPost = page.locator('.css-j7qwjs').first();
@@ -16,6 +16,26 @@ test('add comment', async ({ page }) => {
   const commentText = `This is a test comment (${Date.now()})`;
   await commentInput.fill(commentText);
   await commentInput.press('Enter');
+
+  const newComment = page.getByText(commentText);
+  await expect(newComment).toContainText(commentText);
+});
+
+test('add comment (button press)', async ({ page }) => {
+  await loginFlow(page);
+
+  const firstPost = page.locator('.css-j7qwjs').first();
+  await expect(firstPost).toBeVisible();
+  const postImage = firstPost.locator('.css-1mqut64');
+  await expect(postImage).toBeVisible();
+  await postImage.click();
+
+  const commentInput = page.getByPlaceholder('Comment');
+  await expect(commentInput).toBeVisible();
+
+  const commentText = `This is a test comment (${Date.now()})`;
+  await commentInput.fill(commentText);
+  await page.locator('.css-vb6e92').getByRole('button').click();
 
   const newComment = page.getByText(commentText);
   await expect(newComment).toContainText(commentText);
