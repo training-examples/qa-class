619002c2451db8745843034073d414bdacc1e5b4
Simon <sstur@me.com>
2023-05-26 10:51:12 -0700

    Task 7 solution

--- tests/comment.spec.js
+++ tests/comment.spec.js
@@ -1,4 +1,22 @@
-// Task 7
-// Navigate to a post details page
-// Fill in the comment input and press the submit button
-// Assert that your comment has appeared under the post
+import { test, expect } from '@playwright/test';
+import { loginFlow } from './helpers/login.js';
+
+test('add comment', async ({ page }) => {
+  await loginFlow(page);
+
+  const firstPost = page.locator('.css-j7qwjs').first();
+  await expect(firstPost).toBeVisible();
+  const postImage = firstPost.locator('.css-1mqut64');
+  await expect(postImage).toBeVisible();
+  await postImage.click();
+
+  const commentInput = page.getByPlaceholder('Comment');
+  await expect(commentInput).toBeVisible();
+
+  const commentText = `This is a test comment (${Date.now()})`;
+  await commentInput.fill(commentText);
+  await commentInput.press('Enter');
+
+  const newComment = page.getByText(commentText);
+  await expect(newComment).toContainText(commentText);
+});
